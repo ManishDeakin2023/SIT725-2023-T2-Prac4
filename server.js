207c61d4389b89e4ad962c7acba4da16c5c9d272
Manish.Jadhav <s223541093@deakin.edu.au>
2023-08-07 12:04:58 +1000

    connect mongo

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
+const uri = "mongodb+srv://admin:admin@cluster0.ulnywla.mongodb.net/?retryWrites=true&w=majority"
+let collection;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-
-
-var port = process.env.port || 3000;
-
-app.listen(port,()=> {
-    console.log("App listening to:" + port);
+//this function connects nodejs server
+app.listen(8000, () =>
+{
+    console.log("server started on port 8000");
 })
+
+//this function connects mongoDB server
+async function connect()
+{
+    try {
+        await mongoose.connect(uri)
+        console.log("connected to MongoDB");
+    } catch (err)
+    {
+        console.error(err);
+    }
+}
+connect();
+
+app.get("/", function (req, res) {
+    res.render("index.html");
+})
+
+async function run()
+{
+    try { 
+        collection = mongoose.db().collection('Cats');
+        console.log("done creating collections");
+    } catch (ex)
+    {
+        console.error(ex);
+    }
+     
+}
