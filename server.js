d3c0d0147c353603275ff56e77f7cde4632abf9f
Manish.Jadhav <s223541093@deakin.edu.au>
2023-08-07 13:28:03 +1000

    let me take my lunch

--- server.js
+++ server.js
@@ -1,44 +1,46 @@
 var express = require("express");
-var mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var app = express();
-const uri = "mongodb+srv://admin:admin@cluster0.ulnywla.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://admin:admin@cluster0.ulnywla.mongodb.net/?retryWrites=true&w=majority";
 let collection;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 //this function connects nodejs server
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
+  
+
+//this function connects mongoDB server
+
+async function run() {
+    try {
+      await client.connect();
+      collection = client.db().collection('Cats');
+        console.log("collection has been created");
+        client.close();
+    } catch(ex) {
+      console.error(ex);
+    }
+}
+
+
 app.listen(8000, () =>
 {
     console.log("server started on port 8000");
+    run();
 })
-
-//this function connects mongoDB server
-async function connect()
-{
-    try {
-        await mongoose.connect(uri)
-        console.log("connected to MongoDB");
-    } catch (err)
-    {
-        console.error(err);
-    }
-}
-connect();
-
 app.get("/", function (req, res) {
     res.render("index.html");
 })
 
-async function run()
-{
-    try { 
-        collection = mongoose.db().collection('Cats');
-        console.log("done creating collections");
-    } catch (ex)
-    {
-        console.error(ex);
-    }
-     
-}
